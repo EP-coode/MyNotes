dcd55a869544a11ed03e3181eeb54611b5e1d4f1
EP-coode <ernestprzybyl@gmail.com>
2022-02-16 11:05:06 +0100

    remove app controler

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
@@ -29,9 +27,7 @@ dotenv.config();
     AuthModule,
     NotesModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: AtGuard,
